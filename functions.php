feb58daa19e21c3a905daf881dab79f42fd84559
Ellis Sutehall <ellissutehallx@Omodeleolas-MBP.home>
2018-07-31 17:22:04 +0100

    Change WooCommerce Description Tab heading to Product Overview

--- functions.php
+++ functions.php
@@ -53,3 +53,17 @@ require_once( 'library/responsive-images.php' );
 
 /** If your site requires protocol relative url's for theme assets, uncomment the line below */
 // require_once( 'library/class-foundationpress-protocol-relative-theme-assets.php' );
+
+/**
+ * Rename product data tabs
+ */
+add_filter( 'woocommerce_product_tabs', 'woo_rename_tabs', 98 );
+function woo_rename_tabs( $tabs ) {
+
+	$tabs['description']['title'] = __( 'Product Overview' );		// Rename the description tab
+	// $tabs['reviews']['title'] = __( 'Ratings' );				// Rename the reviews tab
+	// $tabs['additional_information']['title'] = __( 'Product Data' );	// Rename the additional information tab
+
+	return $tabs;
+
+}
