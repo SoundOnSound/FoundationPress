432f407afdcd664dd3f3142741aee5253444c168
SoundOnSound <ellissutehallx@Omodeleolas-MBP.home>
2018-08-02 16:58:47 +0100

    Add Basic sidebar styles. Change 'Tag' to 'Manufacturer' on Product page

--- sidebar.php
+++ sidebar.php
@@ -9,35 +9,45 @@
 ?>
 <aside class="sidebar">
 	<?php dynamic_sidebar( 'sidebar-widgets' ); ?>
+	<!-- Add Custom fields to product pages only -->
+	<?php if( is_product() ): ?>
+		<!-- Add Platform Custom Field -->
+		<?php
+			$platforms = get_field('platforms');
+			if( $platforms ): ?>
+				<div class="sidebar-widgets product-sidebar">
+					<h6>Supported Platforms</h6>
+					<ul>
+						<?php foreach( $platforms as $platform ): ?>
+							<li><?php echo $platform; ?></li>
+						<?php endforeach; ?>
+					</ul>
+				</div>
+			<?php endif; ?>
 
-	<!-- Add Platform Custom Field -->
-	<?php
-		$platforms = get_field('platforms');
-		if( $platforms ):
-	?>
-			<h6>Supported Platforms</h6>
-			<ul>
-				<?php foreach( $platforms as $platform ): ?>
-					<li><?php echo $platform; ?></li>
-				<?php endforeach; ?>
-			</ul>
+		<!-- Add Windows System Requirements from Custom Field -->
+		<?php if( get_field('windows_system_requirements') ): ?>
+			<div class="sidebar-widgets product-sidebar">
+				<h6>Windows System Requirements</h6>
+				<?php the_field('windows_system_requirements'); ?>
+			</div>
 		<?php endif; ?>
 
-	<!-- Add Windows System Requirements from Custom Field -->
-	<?php if( get_field('windows_system_requirements') ): ?>
-		<h6>Windows System Requirements</h6>
-		<?php the_field('windows_system_requirements'); ?>
-	<?php endif; ?>
+		<!-- Add Mac System Requirements from Custom Field -->
+		<?php if( get_field('mac_system_requirements') ): ?>
+			<div class="sidebar-widgets product-sidebar">
+				<h6>Mac System Requirements</h6>
+				<?php the_field('mac_system_requirements'); ?>
+			</div>
+		<?php endif; ?>
 
-	<!-- Add Mac System Requirements from Custom Field -->
-	<?php if( get_field('mac_system_requirements') ): ?>
-		<h6>Mac System Requirements</h6>
-		<?php the_field('mac_system_requirements'); ?>
-	<?php endif; ?>
-
-	<!-- Add SOS Review Link from Custom Field -->
-	<?php if( get_field('sos_review_link') ): ?>
-		<h6>Read The Sound On Sound Review</h6>
-		<a href='<?php the_field('sos_review_link'); ?>' target='_blank'>Read SOS Review</a>
+		<!-- Add SOS Review Link from Custom Field -->
+		<?php if( get_field('sos_review_link') ): ?>
+			<div class="sidebar-widgets product-sidebar">
+				<h6>Read The Sound On Sound Review</h6>
+				<a class="button button-primary expanded sos-review" href='<?php the_field('sos_review_link'); ?>' target='_blank'>Read SOS Review</a>
+			<div>
+		<?php endif; ?>
+		<!-- End If is product -->
 	<?php endif; ?>
 </aside>
