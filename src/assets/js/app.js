1e2b6e2b93203c6468fb39d9f51f3aba8443d62d
Ellis Sutehall <ellissutehall@gmail.com>
2018-08-15 17:42:54 +0100

    Adds homepage styles and flexbox to shop products. Removes 'card' style from products.

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -10,13 +10,13 @@ import Foundation from 'foundation-sites';
 
 $(document).foundation();
 
-// Get all <li> with class of product and assign variable
-let productItem = document.querySelectorAll('li.product');
-// Loop all <li>
-for( let i = 0; i < productItem.length; i++ ) {
-  // Add card class to each item
-  productItem[i].classList.add('card');
-}
+// // Get all <li> with class of product and assign variable
+// let productItem = document.querySelectorAll('li.product');
+// // Loop all <li>
+// for( let i = 0; i < productItem.length; i++ ) {
+//   // Add card class to each item
+//   productItem[i].classList.add('card');
+// }
 
 // Correctly assign last class to last li item on homepage
 let addLastClass = document.querySelector('ul.columns-4 li.product:last-child');
